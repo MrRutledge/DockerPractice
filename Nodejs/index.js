19e5751475194aeb79ed6f7ca62556a1cf6e8f99
MrRutledge <rahimrutledge@gmail.com>
2019-01-07 22:53:29 +0000

    Now using Docker-compose

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hi there! $Sweet Sixteen$ jesus 290');
+    res.send('Hi there! $Sweet Sixteen$ jesus');
 });
 
 app.listen(8082, () => {
